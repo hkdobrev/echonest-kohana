6e17b7571f6f2c0b01822179988a8fc7c443c479
Haralan Dobrev <hkdobrev@gmail.com>
2011-09-14 04:48:25 +0300

    Added Playlist and Search controllers, more routes and a simple view. Playlists now support moods, added actions for extracting list_terms.

--- classes/controller/echonest.php
+++ classes/controller/echonest.php
@@ -1,111 +1,42 @@
 <?php defined('SYSPATH') or die('No direct access allowed!');
 
-class Controller_Echonest extends Controller{
+class Controller_Echonest extends Controller_Template{
 	
 	public function before()
 	{
 		parent::before();
-		$this->api_key = Kohana::$config->load('echonest')->get('api_key');
-		$this->result = '';
-		$this->artist_name = 'oz';
+		$this->template->result = '';
 		$this->echonest = Echonest::instance();
 		$this->session = Session::instance();
 	}
 	
-	protected function _get_artist_id($artists, $artists_count){
+	protected function _get_artist_id($artists, $artists_count, $artist_name){
 		$artist_id = false;
 		$i = -1;
 		while($i < $artists_count - 1 && !$artist_id)
 		{
 			$i = $i + 1;
 			$artist = $artists[$i];
-			if(strtolower($this->artist_name) === strtolower($artist['name'])){
+			if(trim(strtolower($artist_name)) === trim(strtolower($artist['name']))){
 				$artist_id = $artist['id'];
 			}
 		}
 		return $artist_id;
 	}
 	
-	public function action_dynamic(){
-		$artist = str_replace('_', ' ', $this->request->param('artist', $this->artist_name));
-		if($artist){
-			$playlist_api = $this->echonest->getPlaylistApi();
-			$dynamic_playlist = $playlist_api->getDynamic(array('artist' => $artist, 'type' => 'artist-radio'));
-			$this->session->set('dynamic_playlist_id', $dynamic_playlist['session_id']);
-			$this->result = $dynamic_playlist['songs'];
-		}
+	protected function _get_terms($type){
+		$terms = $this->echonest->get('artist/list_terms', array('type' => $type));
+		return $terms['terms'];
 	}
 	
-	public function action_dynamic_rate(){
-		$rating = $this->request->param('rating', 5);
-		if(($session_id = $this->session->get('dynamic_playlist_id'))){
-			$playlist_api = $this->echonest->getPlaylistApi();
-			$next = $playlist_api->getDynamic(array('session_id' => $session_id, 'rating' => $rating));
-			$this->session->set('dynamic_playlist_id', $next['session_id']);
-			$this->result = $next['songs'];
-		} else {
-			throw new Kohana_HTTP_Exception_400('There is no active dynamic playlist for the current user!');
+	public function action_terms(){
+		$type = $this->request->param('type');
+		if($type == 'styles'){
+			$type = 'style';
+		} else{
+			$type = 'mood';
 		}
+		$terms = $this->_get_terms($type);
+		$this->template->result = $terms;
 	}
-	
-	public function action_dynamic_tempo(){
-		$tempo = $this->request->param('tempo', 50) / 50;
-		if(($session_id = $this->session->get('dynamic_playlist_id'))){
-			$playlist_api = $this->echonest->getPlaylistApi();
-			$next = $playlist_api->getDynamic(array('session_id' => $session_id, 'steer' => 'tempo^' . $tempo));
-			$this->session->set('dynamic_playlist_id', $next['session_id']);
-			$this->result = $next['songs'];
-		} else {
-			throw new Kohana_HTTP_Exception_400('There is no active dynamic playlist for the current user!');
-		}
-		
-	}
-	
-	
-	public function action_dynamic_next(){
-		if(($session_id = $this->session->get('dynamic_playlist_id'))){
-			$playlist_api = $this->echonest->getPlaylistApi();
-			$next = $playlist_api->getDynamic(array('session_id' => $session_id));
-			$this->session->set('dynamic_playlist_id', $next['session_id']);
-			$this->result = $next['songs'];
-		} else {
-			throw new Kohana_HTTP_Exception_400('There is no active dynamic playlist for the current user!');
-		}
-	}
-
-
-	public function action_index(){
-		$artistApi = $this->echonest->getArtistApi();
-		$results = $artistApi->search(array('name' => $this->artist_name));
-		$artists_count = count($results);
-		if($artists_count){
-			$artist_id = $this->_get_artist_id($results, $artists_count);
-			if($artist_id){
-				$artist_info = $artistApi->setId($artist_id)->getProfile('songs');
-				$songApi = $this->echonest->getSongApi();
-				$songs = array();
-				foreach ($artist_info['songs'] as $song){
-					$songs[] = $songApi->profile($song['id'], 'song_hotttnesss');
-				}
-				echo 'songs';
-				$this->result = $songs;
-			} else {
-				echo "No matching artist!";
-			}
-		} else {
-			echo 'artist';
-			$this->result = $results;
-		}
-	}
-	
-	public function after()
-	{
-		if($this->result){
-			$this->request->headers('Content-Type', 'application/json');
-			$this->response->body('<pre>' . json_encode($this->result) . '</pre>');
-		}
-		
-		parent::after();
-	}
-	
 }
--- init.php
+++ init.php
@@ -1,28 +1,53 @@
 <?php defined('SYSPATH') or die('No direct access allowed!'); 
 
-Route::set('echonest_dynamic_playlist', 'echonest/dynamic/<artist>')
+Route::set('echonest_search_artist', 'echonest/search/<artist>', array('artist' => '[a-z-_]+'))
 		->defaults(array(
-			'controller' => 'echonest',
+			'directory' => 'echonest',
+			'controller' => 'search',
+			'action' => 'search_by_name'
+		));
+
+Route::set('echonest_dynamic_playlist', 'echonest/playlist/<artist>')
+		->defaults(array(
+			'directory' => 'echonest',
+			'controller' => 'playlist',
 			'action' => 'dynamic',
 			'artist' => false
 		));
 
-Route::set('echonest_dynamic_playlist_next', 'echonest/dynamic/next')
+Route::set('echonest_dynamic_playlist_next', 'echonest/playlist/next')
 		->defaults(array(
-			'controller' => 'echonest',
+			'directory' => 'echonest',
+			'controller' => 'playlist',
 			'action' => 'dynamic_next'
 		));
 
-Route::set('echonest_dynamic_playlist_rate', 'echonest/dynamic/rate/<rating>', array('rating' => '[1-5]'))
+Route::set('echonest_dynamic_playlist_rate', 'echonest/playlist/rate/<rating>', array('rating' => '[1-5]'))
 		->defaults(array(
-			'controller' => 'echonest',
+			'directory' => 'echonest',
+			'controller' => 'playlist',
 			'action' => 'dynamic_rate',
 			'rating' => 5
 		));
 
-Route::set('echonest_dynamic_playlist_tempo', 'echonest/dynamic/tempo/<tempo>', array('tempo' => '([1-9][0-9]?|100)'))
+Route::set('echonest_dynamic_playlist_tempo', 'echonest/playlist/tempo/<tempo>', array('tempo' => '([1-9][0-9]?|100)'))
 		->defaults(array(
-			'controller' => 'echonest',
+			'directory' => 'echonest',
+			'controller' => 'playlist',
 			'action' => 'dynamic_tempo',
 			'tempo' => '50'
+		));
+
+Route::set('echonest_artist_terms', 'echonest/<type>', array('type' => '(moods|styles)'))
+		->defaults(array(
+			'directory' => 'echonest',
+			'controller' => 'playlist',
+			'action' => 'terms'
+		));
+
+Route::set('echonest_dynamic_mood', 'echonest/playlist/mood/<mood>', array('mood' => '[a-z-]+'))
+		->defaults(array(
+			'directory' => 'echonest',
+			'controller' => 'playlist',
+			'action' => 'dynamic_mood'
 		));
